bc200e919b54b7b1fdb0d465621f5c59d0f81122
Conrado <zafra.conrado@gmail.com>
2023-10-07 17:47:49 +0200

    feature(): servicio de llamadas

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -9,12 +9,13 @@ export class HttpService {
   private axios = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com/',
     timeout: 3000,
-    // headers: {'X-Custom-Header': 'foobar'}
   });
 
   getAllUsers = async (): Promise<any> => {
     try {
-      return await this.axios.get('/users');
+      const { data } = await this.axios.get('/users');
+      console.log(data);
+      return data;
     } catch (error) {
       console.log(error);
     }
@@ -22,7 +23,40 @@ export class HttpService {
 
   getOneUser = async (user: string): Promise<any> => {
     try {
-      return await this.axios.get(`/users/${user}`);
+      const { data } = await this.axios.get(`/users/${user}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getAlbums = async (user: string): Promise<any> => {
+    try {
+      const { data } = await this.axios.get(`/users/${user}/albums`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getAlbumPhotos = async (albumId: string): Promise<any> => {
+    try {
+      const { data } = await this.axios.get(`/albums`);
+      const filtered = data.filter((dat: { albumId: string; }) => dat.albumId === albumId);
+
+      localStorage.setItem("recent", JSON.stringify(filtered));
+      
+      return filtered;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getUserToDos = async (user: string): Promise<any> => {
+    try {
+      const { data } = await this.axios.get(`/todos`);
+      const filtered = data.filter((dat: { userId: string; }) => dat.userId === user);
+      return filtered;
     } catch (error) {
       console.log(error);
     }
