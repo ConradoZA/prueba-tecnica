f70af97f1ab844c2a3156a25d82d844fb167cf82
Conrado <zafra.conrado@gmail.com>
2023-10-07 17:48:27 +0200

    chore(): visibilidad del router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
+import es from "@angular/common/locales/es";
+import { registerLocaleData } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NZ_I18N } from "ng-zorro-antd/i18n";
+import { es_ES } from "ng-zorro-antd/i18n";
+import { NzListModule } from "ng-zorro-antd/list";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from "./views/home/home.component";
 import { NotFoundComponent } from "./views/not-found/not-found.component";
 import { UserDetailComponent } from "./views/user-detail/user-detail.component";
 import { UsersListComponent } from "./views/users-list/users-list.component";
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { es_ES } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import es from '@angular/common/locales/es';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 registerLocaleData(es);
 
@@ -24,10 +25,15 @@ registerLocaleData(es);
     UsersListComponent,
     UserDetailComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
-  providers: [
-    { provide: NZ_I18N, useValue: es_ES }
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzListModule,
   ],
+  providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
